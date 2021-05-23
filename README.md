2e549db2dd1013270ea597f905c06d7ca22b0a97
billy rennekamp <billy.rennekamp@gmail.com>
2021-05-23 11:56:34 +0200

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fix eip 2929 with eip 2930
+
+After the berlin hardfork [eip 2929](https://eips.ethereum.org/EIPS/eip-2929) was added which increased the cost for state access opcodes. This broke any contract which used `address.transfer(eth)` to gnosis safes because the hard-coded `2300 gas` was no longer enough to execute the transaction. In order to help contracts that were affected they made [eip 2930](https://eips.ethereum.org/EIPS/eip-2930) which adds an access list to the transaction. Any address added to this access list has gas prepaid on their behalf as part of the total gas calculation. This reduces the gas required when interacting with them which allows the original `2300 gas` to succeed.
+
+> Note: Apparently this only works with [ethers](https://docs.ethers.io/) at the moment.
+
+For our situation we needed to run `endAuction(11000003)` on our auction contract that would allow the transfer to two different gnosis safes to occur.
+
+Look at [./index.js](./index.js) for a complete example.
--- index.js
+++ index.js
@@ -9,39 +9,39 @@ const provider = ethers.getDefaultProvider(network, {
 
 
 async function run() {
-  work = 11000003
   wallet = new ethers.Wallet.fromMnemonic(process.env.MAINNET_MNEMONIC)
   wallet = wallet.connect(provider)
   auction = new ethers.Contract(contracts.ReserveAuction.networks['1'].address, contracts.ReserveAuction.abi, wallet)
 
   overrides = {
-    nonce: 102,
     gasLimit: 500000,
     gasPrice: ethers.utils.parseUnits('150', 'gwei').toString(),
     type: 1,
     accessList: [
       {
-        address: "0x397c2c9c2841bcc396ecaedbc00cd2cfd07de917", // admin
+        address: "0x397c2c9c2841bcc396ecaedbc00cd2cfd07de917", // admin gnosis safe proxy address
         storageKeys: [
             "0x0000000000000000000000000000000000000000000000000000000000000000"
         ]
       },
       {
-        address: "0xaF5c3455A72ecdfc316Bf00e356182B58585B40E", // proceedsRecipient
+        address: "0xaF5c3455A72ecdfc316Bf00e356182B58585B40E", // proceedsRecipient gnosis safe proxy address
         storageKeys: [
             "0x0000000000000000000000000000000000000000000000000000000000000000"
         ]
       },
       {
-        address: '0x34cfac646f301356faa8b21e94227e3583fe3f5f',  // gnosis safe master
+        address: '0x34cfac646f301356faa8b21e94227e3583fe3f5f',  // gnosis safe master address
         storageKeys: []
       }
     ]
   }
+
+  work = 11000003
   endAuctionTX = await auction.endAuction(work, overrides)
   console.log({endAuctionTX})
   resolved = await endAuctionTX.wait()
   console.log({resolved})
 }
 
-// run()
+run()
